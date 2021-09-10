3799db6d361c455c99703d15df3cf6a7dd004543
CloseBuyUK <86831782+CloseBuyUK@users.noreply.github.com>
2021-09-10 23:32:56 +0100

    Initial Commit

---
+++ closebuyv1/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  closebuyv1
+//
+//  Created by Connor A Lynch on 10/09/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ closebuyv1/closebuyv1App.swift
@@ -0,0 +1,17 @@
+//
+//  closebuyv1App.swift
+//  closebuyv1
+//
+//  Created by Connor A Lynch on 10/09/2021.
+//
+
+import SwiftUI
+
+@main
+struct closebuyv1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
